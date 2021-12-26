265c68db119a7f3e968ba9553bf62c1f87ad5a2b
Peter <pdc19@ic.ac.uk>
2021-12-26 23:42:21 +0000

    Another benchmark

--- benchmarking/src/benchmarking.cpp
+++ benchmarking/src/benchmarking.cpp
@@ -8,7 +8,7 @@
 static void BM_gradSinZero(benchmark::State& state) {
   for (auto _ : state) {
     int x = 0;
-  
+
     autodiff::univariate::Function operations;
 
     operations.push(autodiff::operation::Addition(3)); 
@@ -19,6 +19,24 @@ static void BM_gradSinZero(benchmark::State& state) {
   }
 }
 
+
 BENCHMARK(BM_gradSinZero);
+
+static void BM_grad10Sins(benchmark::State& state) {
+  for (auto _ : state) {
+    int x = 0;
+  
+    autodiff::univariate::Function operations;
+
+    for (int i = 0 ; i < 10 ; ++i) {
+      operations.push(autodiff::operation::Sin()); 
+    }
+
+    operations.call(x); // eval operations at x
+    operations.grad(x); // eval operations at grad x
+  }
+}
+
+BENCHMARK(BM_grad10Sins);
 
 BENCHMARK_MAIN();
